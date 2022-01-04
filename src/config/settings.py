92e929276ffc36364bc58c4719b071e1eaea4003
Phai Rotana <59942086+phairotana@users.noreply.github.com>
2022-01-04 21:17:43 +0700

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,9 +7,9 @@ class Setting:
     def get_bind(self):
         return {
             "provider": "postgres",
-            "host": "localhost",
+            "host": "ec2-107-21-146-133.compute-1.amazonaws.com",
             "port": "5432",
-            "user": "postgres",
-            "password": "069099",
-            "database": "loan_db"
+            "user": "agxinrjjbswsui",
+            "password": "fd5b9b89561b1773a3af9158b8dd8827443af29c6ef4fa2c4b47b4ac1f1b87fd",
+            "database": "dblhqgs3tokikk"
         }

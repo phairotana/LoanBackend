bc77f41ecc06f1a424c0454c39ebe0854830bd20
Phai Rotana <59942086+phairotana@users.noreply.github.com>
2022-01-04 21:32:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ app.include_router(schedule.router)
 
 
 if __name__ == "__main__":
-    #uvicorn.run("main:app", host="localhost", port=8000, reload=True)
-    uvicorn.run("main:app", host="global-loan.herokuapp.com", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    #uvicorn.run("main:app", host="global-loan.herokuapp.com", reload=True)

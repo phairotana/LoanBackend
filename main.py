447c914cdcf48fc9787941a9ee6dd7a19421a7ed
phai.rotana <phai.rotana99@gmail.com>
2022-01-04 15:18:10 +0700

    change host name

--- main.py
+++ main.py
@@ -31,5 +31,4 @@ app.include_router(schedule.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
-
+    uvicorn.run("main:app", host="globalloan.netlify.app", port=8000, reload=True)

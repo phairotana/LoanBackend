dca8f3f23518f8efd1cab0e10bdebb8eeb52740f
phai.rotana <phai.rotana99@gmail.com>
2022-01-04 20:43:38 +0700

    Hello

---
+++ src/config/settings.py
@@ -0,0 +1,15 @@
+class Setting:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_bind(self):
+        return {
+            "provider": "postgres",
+            "host": "localhost",
+            "port": "5432",
+            "user": "postgres",
+            "password": "069099",
+            "database": "loan_db"
+        }
